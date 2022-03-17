d1b1cc7df22e673b3f4180c0a6b932bbacc9c0dd
Mayra Bustamante <mayra9919@gmail.com>
2022-03-17 08:01:11 -0500

    remove unnecessary code

--- src/main/java/com/mayra/poem/generator/service/BuildRandomPoemService.java
+++ src/main/java/com/mayra/poem/generator/service/BuildRandomPoemService.java
@@ -13,14 +13,11 @@ public class BuildRandomPoemService {
 
     private static final String RULE_FOR_STARTING_A_POEM = "POEM";
     private static final Random RANDOM = new Random();
+    private static final ThreadLocal<StringBuilder> RANDOM_POEM = new ThreadLocal<>();
     private final Map<String, Rule> poemRules;
-    private final ThreadLocal<StringBuilder> randomPoem;
-    private final ThreadLocal<String> currentRule;
 
     public BuildRandomPoemService(@Qualifier("poemRules") Map<String, Rule> poemRules) {
         this.poemRules = poemRules;
-        randomPoem = new ThreadLocal<>();
-        currentRule = new ThreadLocal<>();
     }
 
     public String execute() {
@@ -29,32 +26,29 @@ public class BuildRandomPoemService {
             throw new ExceptionInInitializerError(
                 "There is an error with the initial configuration");
         }
-        randomPoem.set(new StringBuilder());
-        currentRule.set(firstRule.getId());
+        RANDOM_POEM.set(new StringBuilder());
         for (Action action : firstRule.getActions()) {
             executeAction(action);
         }
-        var finishedRandomPoem = randomPoem.get().toString();
-        randomPoem.remove();
-        currentRule.remove();
+        var finishedRandomPoem = RANDOM_POEM.toString();
+        RANDOM_POEM.remove();
         return finishedRandomPoem.trim();
     }
 
     private void executeAction(Action action) {
         if (null != action.getWords() && action.getWords().length > 0) {
             var rnd = RANDOM.nextInt(action.getWords().length);
-            randomPoem.get().append(action.getWords()[rnd]).append(" ");
+            RANDOM_POEM.get().append(action.getWords()[rnd]).append(" ");
             return;
         }
         if (null != action.getAssociatedRules() && action.getAssociatedRules().length > 0) {
             var rnd = RANDOM.nextInt(action.getAssociatedRules().length);
             var selectedRule = action.getAssociatedRules()[rnd];
-            currentRule.set(selectedRule);
             if (RuleConstants.KEYWORD_END.equals(selectedRule)) {
                 return;
             }
             if (RuleConstants.KEYWORD_LINE_BREAK.equals(selectedRule)) {
-                randomPoem.get().append(System.lineSeparator());
+                RANDOM_POEM.get().append(System.lineSeparator());
                 return;
             }
             executeRule(selectedRule);
